4014093c47ac36641b310c0eac189d3dbe2f9966
Yukio Fukuzawa <nishinokaze8@gmail.com>
2015-07-04 19:36:54 +1200

    forking repo requires internal code changes

--- trackerapi/me.go
+++ trackerapi/me.go
@@ -8,8 +8,8 @@ import (
 	"os"
 	u "os/user"
 
-	"github.com/GoBootcamp/clirescue/cmdutil"
-	"github.com/GoBootcamp/clirescue/user"
+	"github.com/fzyukio/clirescue/cmdutil"
+	"github.com/fzyukio/clirescue/user"
 )
 
 var (
